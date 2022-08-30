50635576857fd9b10b59ba22dee4d575f7f8479b
YorkDau <51653605+YorkDau@users.noreply.github.com>
2022-08-30 17:43:59 -0500

    Avance -> Validacion del formulario de Contratos

--- src/pages/Propuesta/PropuestasModalCreateOrUpdate.jsx
+++ src/pages/Propuesta/PropuestasModalCreateOrUpdate.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useMemo, useState, useEffect } from 'react';
 import NiceModal, { useModal } from '@ebay/nice-modal-react';
 import {
   Box,
@@ -9,13 +10,89 @@ import {
   Stack,
   Typography
 } from '@mui/material';
+import { Formik } from 'formik';
 import ActionCerrar from '../../components/ButtonsAction/ActionCerrar';
 import SelectCommon from '../../components/SelectCommon';
+import EstadosServices from '../../services/estadosServices';
 import TextFieldCommon from '../../components/TextFieldCommon';
 import ButtonCommon from '../../components/ButtonCommon';
+import DatePickerCommon from '../../components/Datepicker';
+import EmpresasServices from '../../services/EmpresasServices';
+import InstitucionesServices from '../../services/InstitucionesServices';
+import validationSchemeDataProuesta from './ValidationSchemeDataPropuesta';
 
 function PropuestasModalCreateOrUpdate() {
+  const [info, setInfo] = useState(null);
+  const [estados, setEstados] = useState([]);
+  const [empresas, setEmpresas] = useState([]);
+  const [instituciones, setInstituciones] = useState([]);
+  const [data, setData] = useState(null);
+  async function fechDataEstados() {
+    try {
+      const response = await EstadosServices.get();
+      if (response.status === 200) {
+        setEstados(
+          response.data.map((item) => ({ label: item.name, value: item.id }))
+        );
+      }
+    } catch (error) {
+      setInfo({
+        type: 'error',
+        message: 'se ha producido un error, por favor intentelo más tarde.'
+      });
+    } finally {
+      // setLoading(null);
+    }
+  }
+  const tiposContratos = useMemo(
+    () => [
+      { label: 'CONTRATO PAE', value: 'PAE' },
+      { label: 'CONTRATO OBRAS', value: 'OBRAS' }
+    ],
+    []
+  );
   const modal = useModal();
+  async function fechDataEmpresa() {
+    try {
+      const response = await EmpresasServices.get();
+      if (response.status === 200) {
+        setEmpresas(
+          response.data.data.map((item) => ({
+            label: item.nombre,
+            value: item.id
+          }))
+        );
+      }
+    } catch (error) {
+      setInfo({
+        type: '',
+        message: ''
+      });
+    }
+  }
+  async function fechDataInstituciones() {
+    try {
+      const response = await InstitucionesServices.get();
+      if (response.status === 200) {
+        setInstituciones(
+          response.data.data.map((item) => ({
+            label: item.nombre,
+            value: item.id
+          }))
+        );
+      }
+    } catch (error) {
+      setInfo({
+        type: '',
+        message: ''
+      });
+    }
+  }
+  useEffect(() => {
+    fechDataEstados();
+    fechDataEmpresa();
+    fechDataInstituciones();
+  }, []);
   return (
     <Dialog
       fullScreen={false}
@@ -44,95 +121,190 @@ function PropuestasModalCreateOrUpdate() {
         Crear Propuesta
       </DialogTitle>
       <DialogContent>
-        <Typography
-          sx={{
-            fontSize: '16px',
-            padding: '0px 30px',
-            margin: '0px 10px',
-            color: '#6D66CC'
+        <Formik
+          validationSchema={validationSchemeDataProuesta}
+          enableReinitialize
+          initialValues={
+            data || {
+              empresaBeneficiaria: '',
+              empresaContratista: '',
+              numero_propuesta: '',
+              nombre: '',
+              estado: '',
+              fecha_inicial: '',
+              tipo: '',
+              institucion_id: ''
+            }
+          }
+          onSubmit={(values, { resetForm }) => {
+            console.log(values);
+            resetForm();
           }}
         >
-          Datos de la propuesta-Contrato
-        </Typography>
-        <Box sx={{ padding: '10px 40px 0px 40px ' }}>
-          <Grid container direction="row" spacing={2} marginBottom={2}>
-            <Grid item lg={4}>
-              <SelectCommon required label="Departamento" onChange={() => {}} />
-            </Grid>
-            <Grid item lg={4}>
-              <SelectCommon required label="Municipio" onChange={() => {}} />
-            </Grid>
-            <Grid item lg={4}>
-              <SelectCommon
-                required
-                label="Tipo de contrato"
-                onChange={() => {}}
-              />
-            </Grid>
-          </Grid>
-          <Grid container direction="row" spacing={2} marginBottom={2}>
-            <Grid item lg={4}>
-              <TextFieldCommon
-                required
-                label="Número del contrato"
-                onChange={() => {}}
-              />
-            </Grid>
-            <Grid item lg={4}>
-              <TextFieldCommon
-                required
-                label="Nombre del contrato"
-                onChange={() => {}}
-              />
-            </Grid>
-            <Grid item lg={4}>
-              <SelectCommon required label="Estado" onChange={() => {}} />
-            </Grid>
-          </Grid>
-        </Box>
-        <Typography
-          sx={{
-            fontSize: '16px',
-            padding: '0px 30px',
-            margin: '0px 10px',
-            color: '#6D66CC'
-          }}
-        >
-          Empresa a realizar el contrato
-        </Typography>
-        <Box sx={{ padding: '10px 40px 0px 40px ' }}>
-          <SelectCommon required label="Empresa" />
-        </Box>
-        <Typography
-          sx={{
-            fontSize: '16px',
-            padding: '0px 30px',
-            margin: '0px 10px',
-            color: '#6D66CC'
-          }}
-        >
-          Empresa que realiza el contrato
-        </Typography>
-        <Box sx={{ padding: '10px 40px 0px 40px ' }}>
-          <SelectCommon required label="Empresa" />
-        </Box>
+          {(formik) => (
+            <form onSubmit={formik.handleSubmit}>
+              <Typography
+                sx={{
+                  fontSize: '16px',
+                  padding: '0px 30px',
+                  margin: '0px 10px',
+                  color: '#6D66CC'
+                }}
+              >
+                Empresa Beneficiaria.
+              </Typography>
+              <Box sx={{ padding: '0px 40px 10px 40px ' }}>
+                <SelectCommon
+                  options={empresas}
+                  value={formik.values.empresaBeneficiaria}
+                  error={
+                    formik.touched.empresaBeneficiaria &&
+                    formik.errors.empresaBeneficiaria
+                  }
+                  required
+                  label="Empresa"
+                  onChange={(empresaBeneficiaria) => {
+                    formik.setFieldValue(
+                      'empresaBeneficiaria',
+                      empresaBeneficiaria
+                    );
+                  }}
+                />
+              </Box>
+              <Typography
+                sx={{
+                  fontSize: '16px',
+                  padding: '0px 30px',
+                  margin: '0px 10px',
+                  color: '#6D66CC'
+                }}
+              >
+                Empresa contratista
+              </Typography>
+              <Box sx={{ padding: '0px 40px 0px 40px ' }}>
+                <SelectCommon
+                  options={empresas}
+                  value={formik.values.empresaContratista}
+                  error={
+                    formik.touched.empresaContratista &&
+                    formik.errors.empresaContratista
+                  }
+                  required
+                  label="Empresa"
+                  onChange={(empresaContratista) => {
+                    formik.setFieldValue(
+                      'empresaContratista',
+                      empresaContratista
+                    );
+                  }}
+                />
+              </Box>
+              <Box sx={{ padding: '10px 40px 0px 40px ' }}>
+                <Grid container direction="row" spacing={2} marginBottom={2}>
+                  <Grid item lg={4}>
+                    <TextFieldCommon
+                      required
+                      value={formik.values.numero_propuesta}
+                      name="numero_propuesta"
+                      error={
+                        formik.touched.numero_propuesta &&
+                        formik.errors.numero_propuesta
+                      }
+                      label="Número del contrato"
+                      onChange={formik.handleChange}
+                    />
+                  </Grid>
+                  <Grid item lg={8}>
+                    <TextFieldCommon
+                      required
+                      value={formik.values.nombre}
+                      error={formik.touched.nombre && formik.errors.nombre}
+                      onChange={formik.handleChange}
+                      name="nombre"
+                      label="Nombre del contrato"
+                    />
+                  </Grid>
+                </Grid>
+
+                <Grid container direction="row" spacing={2} marginBottom={2}>
+                  <Grid item lg={4}>
+                    <SelectCommon
+                      options={tiposContratos}
+                      required
+                      value={formik.values.tipo}
+                      error={formik.touched.tipo && formik.errors.tipo}
+                      label="Tipo de contrato"
+                      onChange={(tipo) => {
+                        formik.setFieldValue('tipo', tipo);
+                      }}
+                    />
+                  </Grid>
+                  <Grid item lg={4}>
+                    <DatePickerCommon
+                      error={
+                        formik.touched.fecha_inicial &&
+                        formik.errors.fecha_inicial
+                      }
+                      value={formik.values.fecha_inicial}
+                      required
+                      label="Fecha Inicial"
+                      onChange={(fecha) => {
+                        formik.setFieldValue('fecha_inicial', fecha);
+                      }}
+                    />
+                  </Grid>
+                  <Grid item lg={4}>
+                    <SelectCommon
+                      options={estados}
+                      value={formik.values.estado}
+                      required
+                      error={formik.touched.estado && formik.errors.estado}
+                      label="Estado"
+                      onChange={(estado) => {
+                        formik.setFieldValue('estado', estado);
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+              </Box>
+              {formik.values.tipo.value === 'PAE' && (
+                <Box sx={{ padding: '0px 40px 0px 40px ' }}>
+                  <SelectCommon
+                    options={instituciones}
+                    required
+                    error={
+                      formik.touched.institucion_id &&
+                      formik.errors.institucion_id
+                    }
+                    value={formik.values.institucion_id}
+                    label="Institucion"
+                    onChange={(institucion) => {
+                      formik.setFieldValue('institucion_id', institucion);
+                    }}
+                  />
+                </Box>
+              )}
+
+              <Stack
+                spacing={2}
+                direction="row"
+                sx={{ margin: '20px', justifyContent: 'center' }}
+              >
+                <ButtonCommon
+                  type="button"
+                  variant="outlined"
+                  onClick={() => {
+                    modal.hide();
+                  }}
+                >
+                  CANCELAR
+                </ButtonCommon>
+                <ButtonCommon type="submit">GUARDAR</ButtonCommon>
+              </Stack>
+            </form>
+          )}
+        </Formik>
       </DialogContent>
-      <Stack
-        spacing={2}
-        direction="row"
-        sx={{ margin: '20px', justifyContent: 'center' }}
-      >
-        <ButtonCommon
-          type="button"
-          variant="outlined"
-          onClick={() => {
-            modal.hide();
-          }}
-        >
-          CANCELAR
-        </ButtonCommon>
-        <ButtonCommon type="submit">GUARDAR</ButtonCommon>
-      </Stack>
     </Dialog>
   );
 }
---
+++ src/pages/Propuesta/ValidationSchemeDataPropuesta.js
@@ -0,0 +1,23 @@
+import * as Yup from 'yup';
+
+const validationSchemeDataProuesta = Yup.object().shape({
+  empresaBeneficiaria: Yup.object().required(
+    'La empresa beneficiaria es requerida'
+  ),
+  empresaContratista: Yup.object().required(
+    'La empresa Contratista es requerida'
+  ),
+  numero_propuesta: Yup.string().required(
+    'El numero del contrato es requerido'
+  ),
+  fecha_inicial: Yup.date().required('La fecha es requerida'),
+  estado: Yup.object().required('El estado es requerido'),
+  nombre: Yup.string().required('El nombre del contrato es requerido'),
+  tipo: Yup.object().required('El tipo de contrato es requerido'),
+  institucion_id: Yup.object().when('tipo', (tipo) => {
+    if (tipo && tipo.value === 'PAE') {
+      return Yup.object().required('La institucion es requerida');
+    }
+  })
+});
+export default validationSchemeDataProuesta;
