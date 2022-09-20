f1302960cffc6afd12731a6c99d86743300ec459
Camilo Colón <51205231+colonca@users.noreply.github.com>
2022-09-20 00:19:09 -0500

    update keycloack ssl

--- src/utils/keycloack.js
+++ src/utils/keycloack.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://18.221.165.181:8085/auth',
+  url: 'https://keycloak.fundacionarbolprospero.com//auth',
   realm: 'funarpro',
   clientId: 'funarpro-web'
 });
